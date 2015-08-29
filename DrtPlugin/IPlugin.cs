ae4ca940e55b0680cb52485c33374bf43a934d81
drOpen <kudryashov.andrey@gmail.com>
2015-08-30 02:56:23 +0300

    [+] IPlugin interface [+] DDEventArgs - support dictonary

---
+++ DrtPlugin/IPlugin.cs
@@ -0,0 +1,51 @@
+﻿/*
+  IPlugin.cs -- Interface describes the base class 1.0.0, August 30, 2015
+ 
+  Copyright (c) 2013-2015 Kudryashov Andrey aka Dr
+ 
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+      1. The origin of this software must not be misrepresented; you must not
+      claim that you wrote the original software. If you use this software
+      in a product, an acknowledgment in the product documentation would be
+      appreciated but is not required.
+
+      2. Altered source versions must be plainly marked as such, and must not be
+      misrepresented as being the original software.
+
+      3. This notice may not be removed or altered from any source distribution.
+
+      Kudryashov Andrey <kudryashov.andrey at gmail.com>
+
+ */
+
+using DrOpen.DrCommon.DrData;
+using System;
+
+namespace DrOpen.DrTask.DrtPlugin
+{
+    public interface IPlugin
+    {
+        /// <summary>
+        /// raise event before execute plugin
+        /// </summary>
+        event EventHandler BeforeExecute;
+        /// <summary>
+        /// raise event before after plugin
+        /// </summary>
+        event EventHandler AfterExecute;
+        /// <summary>
+        /// Invokes execution of plugin with given config and data
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="nodes"></param>
+        /// <returns>This method returns an error code</returns>
+        DDNode Execute(DDNode config, params DDNode[] nodes);
+    }
+}
