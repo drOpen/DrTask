8086c6d5c2c44a4a97ac804b9b1cb6c65020a603
kotwin <kotwinn@gmail.com>
2015-08-31 18:44:49 +0300

    add custom event data classes

---
+++ DrtPlugin/DrtEventArgs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DrOpen.DrCommon.DrData;
+
+namespace DrOpen.DrTask.DrtPlugin
+{
+    /// <summary>
+    /// Class for transfering event information in form of DDNode
+    /// </summary>
+    public class DrtEventArgs : EventArgs
+    {
+        public DDNode EventData { get; set; }
+
+        public DrtEventArgs() { }
+
+        public DrtEventArgs(DDNode EventData)
+        {
+            this.EventData = EventData;
+        }
+    }
+
+    /// <summary>
+    /// Extension of DrtEventArgs with flag that indicates if plugin doesn't have to be executed
+    /// </summary>
+    public class DrtEventCancelArgs : DrtEventArgs
+    {
+        public DrtEventCancelArgs()
+        {
+            this.Cancel = false;
+        }
+
+        public bool Cancel { get; set; }
+    }
+}
